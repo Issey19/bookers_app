05f0788b65d8e51831cade37bee32ff3ef61158d
Issey19 <aaaif342@gmail.com>
2021-10-27 12:22:39 +0000

    コントローラー更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,51 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
-  def show
+
+  def create
+  @book = Book.new
+  @book.assign_attributes(book_params)
+  if @book.valid?
+     @book.save!
+    redirect_to action: :index
+  else
+    @books = Book.all
+    render action: :index
+  end
   end
 
-  def new
+def edit
+  @book = Book.find(params[:id])
+  @book.assign_attributes(book_params) 
+  if @book.valid?
+    @book.save!
+    redirect_to action: :index
+  else
+    @books = Book.all
+    render action: :new
+  end
+end
+
+  def destroy
+  @book = Book.find(params[:id])
+  @book.destroy!
+  redirect_to action: :index
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-end
+
+
+
+private
+
+def book_params
+  params.fetch(:form, {}).permit(
+      :title,
+      :body,
+  )
+  end
