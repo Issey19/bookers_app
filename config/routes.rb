42a036ed85d5d7859ec11fc03b6765e3fddf60ac
Issey19 <aaaif342@gmail.com>
2021-10-28 14:52:06 +0000

    new更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
+  resources :books
   get 'books/index'
   get 'books/show'
   get 'books/new'
-  get 'books/edit'
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/new'
-  get 'homes/edit'
+  get 'books' => 'books#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
